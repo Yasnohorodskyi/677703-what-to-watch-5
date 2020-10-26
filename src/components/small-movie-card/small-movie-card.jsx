e5dcba4513e3ba7157d286de6e6bf86ebc050e35
Dmitriy Teterin <teterinda@gmail.com>
2020-10-26 20:55:32 +0300

    Редактируем компонент SmallMovieCard для взаимодействия с компонентом VideoPlayer

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -1,10 +1,19 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {filmType} from "../../custom-prop-types.js";
+import VideoPlayer from "../video-player/video-player.jsx";
+
+const PLAYING_TIMEOUT = 1000;
 
 class SmallMovieCard extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      videoRef: null,
+      // timer: null,
+    };
+    this._timer = null;
   }
 
   render() {
@@ -12,20 +21,38 @@ class SmallMovieCard extends PureComponent {
     const {
       title,
       coverImg,
-      id
+      id,
+      video,
     } = film;
 
     return (
       <article className="small-movie-card catalog__movies-card"
         onMouseOver={() => {
           onMouseOver(id);
+          const timer = setTimeout(() => {
+            this.state.videoRef.play();
+          }, PLAYING_TIMEOUT);
+          this._timer = timer;
+        }}
+        onMouseOut={() => {
+          if (this._timer) {
+            this.state.videoRef.load();
+          }
+          clearTimeout(this._timer);
         }}
         onClick={() => {
           onMovieCardClick(id);
         }}
       >
         <div className="small-movie-card__image">
-          <img src={`/img/${coverImg}`} alt={title} width="280" height="175" />
+          <VideoPlayer
+            src={video}
+            coverImg={coverImg}
+            onVideoMount={(videoRef) => {
+              this.setState({videoRef});
+            }}
+          />
+          {/* <img src={`/img/${coverImg}`} alt={title} width="280" height="175" /> */}
         </div>
         <h3 className="small-movie-card__title">
           <a className="small-movie-card__link" href="movie-page.html">{title}</a>
