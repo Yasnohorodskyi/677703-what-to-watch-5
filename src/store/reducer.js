0819377166e566c0c40830a0d8df943f787f92a9
Dmitriy Teterin <teterinda@gmail.com>
2020-11-15 15:43:30 +0300

    Добавляет в редьюсер обработку новых действий. Также создает адаптер для полученных фильмов.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,34 @@
 import {extend} from "../utils.js";
 import {ActionType} from "./action.js";
-// import allFilms from "../mocks/films.js";
 import allReviews from "../mocks/reviews.js";
+
+const adaptFilm = (film) => ({
+  id: film[`id`],
+  title: film[`name`],
+  fullImg: film[`poster_image`],
+  coverImg: film[`preview_image`],
+  backgroundImage: film[`background_image`],
+  backgroundColor: film[`background_color`],
+  video: film[`video_link`],
+  previewVideoLink: film[`video_link`],
+  description: film[`description`],
+  rating: film[`rating`],
+  scoresCount: film[`scores_count`],
+  director: film[`director`],
+  starring: film[`starring`],
+  duration: film[`run_time`],
+  genre: film[`genre`],
+  releaseDate: film[`released`],
+  isFavorite: film[`is_favorite`],
+  similarFilmsID: [3, 7, 9],
+});
 
 const getGenres = (films) => {
   const genres = [`All genres`];
+
+  if (films.length === 0) {
+    return genres;
+  }
 
   films.forEach((film) => {
     if (!genres.includes(film.genre)) {
@@ -15,13 +39,11 @@ const getGenres = (films) => {
   return genres;
 };
 
-const allGenres = getGenres(allFilms);
-
 const initialState = {
   genre: `All genres`,
   allFilms: [],
   allGenres: [],
-  allReviews: [],
+  allReviews,
   genreFilms: [],
 };
 
@@ -34,13 +56,23 @@ const reducer = (state = initialState, action) => {
     case ActionType.SET_GENRE_FILMS:
       if (!state.genre || state.genre === `All genres`) {
         return extend(state, {
-          genreFilms: allFilms,
+          genreFilms: state.allFilms,
         });
       }
-      const genreFilms = allFilms.filter((film) => film.genre === state.genre);
+      const genreFilms = state.allFilms.filter((film) => film.genre === state.genre);
 
       return extend(state, {
         genreFilms,
+      });
+    case ActionType.LOAD_FILMS:
+      const sourceFilms = action.payload;
+      const allFilms = sourceFilms.map((film) => adaptFilm(film));
+      return extend(state, {
+        allFilms,
+      });
+    case ActionType.SET_ALL_GENRES:
+      return extend(state, {
+        allGenres: getGenres(state.allFilms),
       });
   }
 
