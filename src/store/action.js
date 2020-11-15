22622c619e9354b2e8794b9800f05c2f50d628d4
Dmitriy Teterin <teterinda@gmail.com>
2020-11-15 15:41:15 +0300

    Разбивает ActionCreator на отдельные функции. Добавляет новые actions и их функции.

--- src/store/action.js
+++ src/store/action.js
@@ -1,16 +1,25 @@
 export const ActionType = {
   CHANGE_ACTIVE_GENRE: `CHANGE_ACTIVE_GENRE`,
   SET_GENRE_FILMS: `SET_GENRE_FILMS`,
+  LOAD_FILMS: `LOAD_FILMS`,
+  SET_ALL_GENRES: `SET_ALL_GENRES`,
 };
 
-export const ActionCreator = {
-  changeActiveGenre: (genre) => ({
-    type: ActionType.CHANGE_ACTIVE_GENRE,
-    genre,
-  }),
-  setGenreFilms: () => {
-    return {
-      type: ActionType.SET_GENRE_FILMS,
-    };
-  },
-};
+export const changeActiveGenre = (genre) => ({
+  type: ActionType.CHANGE_ACTIVE_GENRE,
+  genre,
+});
+
+
+export const setGenreFilms = () => ({
+  type: ActionType.SET_GENRE_FILMS,
+});
+
+export const setAllGenres = () => ({
+  type: ActionType.SET_ALL_GENRES,
+});
+
+export const loadFilms = (films) => ({
+  type: ActionType.LOAD_FILMS,
+  payload: films,
+});
