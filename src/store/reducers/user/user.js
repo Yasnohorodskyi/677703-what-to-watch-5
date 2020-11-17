5724f49e744642f1bf41138592d434624e7a51a0
Dmitriy Teterin <teterinda@gmail.com>
2020-11-17 20:32:32 +0300

    Создаст новый файл-редьюсер reducer/user/user.js отвечающий за обработку состояния авторизации

---
+++ src/store/reducers/user/user.js
@@ -0,0 +1,19 @@
+import {AuthorizationStatus} from "../../../const.js";
+import {ActionType} from "../../action.js";
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRE_AUTHORIZATION:
+      return Object.assign({}, state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+export {user};
