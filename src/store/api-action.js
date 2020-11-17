204c298219dfcce6454c43d343a62f3d88dcd164
Dmitriy Teterin <teterinda@gmail.com>
2020-11-17 20:30:07 +0300

    Опишет обработку функций авторизации

--- src/store/api-action.js
+++ src/store/api-action.js
@@ -1,6 +1,24 @@
-import {loadFilms} from "./action.js";
+import {AuthorizationStatus} from "../const.js";
+import {loadFilms, requireAuthorization} from "./action.js";
 
 export const fetchFilmsList = () => (dispatch, __getState, api) => (
   api.get(`/films`)
       .then(({data}) => dispatch(loadFilms(data)))
+);
+
+export const checkAuth = () => (dispatch, __getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({login: email, password}) => (dispatch, __getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+);
+
+export const logout = () => (dispatch, __getState, api) => (
+  api.get(`/logout`)
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH)))
+    .catch(() => {})
 );
