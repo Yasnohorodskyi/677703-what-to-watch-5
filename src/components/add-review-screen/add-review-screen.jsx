63e9389826389cf53d5040a8ad7062534e5f2ba0
Dmitriy Teterin <teterinda@gmail.com>
2020-11-20 16:04:24 +0300

    Изменит ссылку для перехода на главный экран на странице добавления нового комментария

--- src/components/add-review-screen/add-review-screen.jsx
+++ src/components/add-review-screen/add-review-screen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AddReviewForm from "../add-review-form/add-review-form";
+import {Link} from "react-router-dom";
 import withFormHanling from "../../hocs/with-form-handling/with-form-handling";
 
 const AddReviewFormWrapped = withFormHanling(AddReviewForm);
@@ -16,11 +17,11 @@ const AddReviewScreen = () => {
 
         <header className="page-header">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <Link to={`/`} className="logo__link">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <nav className="breadcrumbs">
