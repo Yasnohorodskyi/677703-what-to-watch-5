c978ca007603ee8dfcb32773482d8b9a07fd2c6c
Dmitriy Teterin <teterinda@gmail.com>
2020-12-02 15:12:59 +0300

    Добавит unit-тесты для reducer load-data

--- src/const.js
+++ src/const.js
@@ -52,14 +52,35 @@ export const mockFilm = {
   backgroundColor: `#ffffff`,
   video: `https://some-link`,
   previewVideoLink: `https://some-link`,
-  description: `In the 1930s, the Grand Budapest Hotel is a popular       European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+  description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
   rating: 8.9,
   scoresCount: 240,
   starring: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+  director: `Wes Andreson`,
   duration: 99,
   genre: `Comedy`,
   releaseDate: 2014,
   isFavorite: false,
+};
+
+export const mockSourceFilm = {
+  "id": 1,
+  "name": `The Grand Budapest Hotel`,
+  "poster_image": `img/the-grand-budapest-hotel-poster.jpg`,
+  "preview_image": `img/the-grand-budapest-hotel.jpg`,
+  "background_image": `img/the-grand-budapest-hotel-bg.jpg`,
+  "background_color": `#ffffff`,
+  "video_link": `https://some-link`,
+  "preview_video_link": `https://some-link`,
+  "description": `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+  "rating": 8.9,
+  "scores_count": 240,
+  "director": `Wes Andreson`,
+  "starring": [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+  "run_time": 99,
+  "genre": `Comedy`,
+  "released": 2014,
+  "is_favorite": false
 };
 
 export const mockReview = {
@@ -69,6 +90,17 @@ export const mockReview = {
   rating: 8.9,
   text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
   date: `2019-05-08T14:13:56.569Z`,
+};
+
+export const mockSourceReview = {
+  "id": 1,
+  "user": {
+    "id": 4,
+    "name": `Kate Muir`
+  },
+  "rating": 8.9,
+  "comment": `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+  "date": `2019-05-08T14:13:56.569Z`,
 };
 
 export const mockGenres = [`All genres`, `Comedy`, `Adventure`, `Triller`];
